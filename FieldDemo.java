c0cb02f281f9c1b8db60d67e8ca4233ad4ebf66f
Harshini <harshini.muruganathan@gmail.com>
2026-06-16 15:28:10 +0530

    Ques 1

---
+++ FieldDemo.java
@@ -0,0 +1,25 @@
+public class FieldDemo {
+    int age;
+    double salary;
+    boolean active;
+    String name;
+    public static void main(String[] args) {
+        FieldDemo obj = new FieldDemo();
+        System.out.println("age = " + obj.age);
+        System.out.println("salary = " + obj.salary);
+        System.out.println("active = " + obj.active);
+        System.out.println("name = " + obj.name);
+        int x = 10;
+        System.out.println("x = " + x);
+        /*
+        int y;
+        System.out.println(y);
+        */
+        /*
+        int 1age;         
+        String student-name;
+        double class;      
+        boolean @active;  
+        */
+    }
+}
