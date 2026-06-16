1cbaa3c9e2e15bd0aace8109fa75bfc57dc7eff0
Harshini <harshini.muruganathan@gmail.com>
2026-06-16 19:19:35 +0530

    Ques 2

---
+++ SumPrimeNumbers.java
@@ -0,0 +1,20 @@
+public class SumPrimeNumbers {
+     public static void main(String[] args) {
+        int count = 0, num = 2, sum = 0;
+    while (count < 100) {
+            boolean prime = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime) {
+                sum += num;
+                count++;
+            }
+            num++;
+        }
+        System.out.println("Sum of the first 100 prime numbers: " + sum);
+    }
+}
