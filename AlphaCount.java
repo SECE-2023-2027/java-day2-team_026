48198ae1fd8a42037da5550c2df09e90fcf0b82d
Yunus <yunus.mm2023csbs@sece.ac.in>
2026-06-16 21:30:58 +0530

    Ques 15

---
+++ AlphaCount.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AlphaCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String compressed = "";
+        int count = 1;
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                compressed += str.charAt(i);
+                compressed += count;
+                count = 1;
+            }
+        }
+        System.out.println(compressed);
+        sc.close();
+    }
+}
